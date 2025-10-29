2205a041da94bf0f9bbe7cfd556fe548a512d44c
Italo Polo <italopolo1@outlook.com>
2025-10-29 17:15:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// script básico para futuros efeitos suaves e animações
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Site Dhiuliani Silvestre carregado com sucesso!");
+});
